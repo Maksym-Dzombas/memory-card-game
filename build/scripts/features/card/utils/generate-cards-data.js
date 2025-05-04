a9a07ea126fddae81f08f43823c860201bbae563
Maksym Dzombas <dzombasmaksimv@gmail.com>
2025-05-04 19:35:21 +0200

    add random images gem

--- build/scripts/features/card/utils/generate-cards-data.js
+++ build/scripts/features/card/utils/generate-cards-data.js
@@ -2,13 +2,16 @@ import { crypt } from "../../../../consts/template-game.js";
 import { gemsImage } from "../../../../consts/gems-image.js";
 import { urlImages } from "../../../../consts/url-images.js";
 import { cardsGemStore } from "../../../../store/cards.js";
+import { randomlyArray } from "./randomly-array.js";
 export const generateCards = (count) => {
     const cardsGem = [];
+    const randomImagesGem = randomlyArray(gemsImage);
+    console.log(randomImagesGem);
     for (let index = 0; index < crypt[count]; index++) {
         const cardGem = {
             id: index + 1,
             name: `gem-${index + 1}`,
-            image: urlImages + "gems/" + gemsImage[index]
+            image: urlImages + "gems/" + randomImagesGem[index]
         };
         cardsGem.push(cardGem);
     }
@@ -16,7 +19,7 @@ export const generateCards = (count) => {
         const cardGem = {
             id: index + 1,
             name: `gem-${index + 1}`,
-            image: urlImages + "gems/" + gemsImage[index]
+            image: urlImages + "gems/" + randomImagesGem[index]
         };
         cardsGem.push(cardGem);
     }
